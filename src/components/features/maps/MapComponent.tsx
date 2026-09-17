b9822afb4c67ba8915f01a5ecc70a19ddd6b8a31
Jae Young Seo <jae.jy.seo@gmail.com>
2026-09-17 18:08:11 -0400

    bug fixing api key call to carto

--- src/components/features/maps/MapComponent.tsx
+++ src/components/features/maps/MapComponent.tsx
@@ -107,7 +107,7 @@ export default function MapComponent({
       className="h-full w-full z-0"
     >
       <TileLayer
-        url={`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png?api_key=${CARTO_API_KEY}`}
+        url={`https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png?key=${CARTO_API_KEY}`}
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions" target="_blank" rel="noopener noreferrer">CARTO</a>'
       />
       {!isMobile && <ZoomControl position="topright" />}
